b18cf81c2e75cefa3689af068918d055d88263c5
Rodrigo <sschuckk@gmail.com>
2024-03-27 21:23:26 -0400

    Add docstring for 'CucumberTestRunner.java' file

--- src/test/java/runner/CucumberTestRunner.java
+++ src/test/java/runner/CucumberTestRunner.java
@@ -4,16 +4,18 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
+/**
+ * This class serves as the TestNG runner class for executing Cucumber tests.
+ */
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = {"org/schuck/step_definitions"},
         plugin = {"pretty", "html:target/TestReport.html", "json:target/cucumber.json"},
         monochrome = true,
         publish = true)
-
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
-
-//    @Override
+    // TODO: Verify why cant run test in parallel
+    // @Override
     @DataProvider(parallel = true)
     public Object[][] scenario() {
         return super.scenarios();
